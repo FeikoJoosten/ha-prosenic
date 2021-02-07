0fdb7b9908d44fa5c0c87116724d25c69313e21b
Feiko Joosten <feiko_joosten@hotmail.com>
2021-02-07 16:30:52 +0100

    different raw command support

--- custom_components/prosenic/vacuum.py
+++ custom_components/prosenic/vacuum.py
@@ -317,8 +317,9 @@ class ProsenicVacuum(StateVacuumEntity):
     
     async def async_send_command(self, command, params=None, **kwargs):
         """Send raw command."""
+            _LOGGER.warning("Raw Command: (%s) VS (%s)", command, SERVICE_SET_WATER_SPEED)
         try:
-            if command is not None and command == SERVICE_SET_WATER_SPEED:
+            if command == SERVICE_SET_WATER_SPEED:
                 await self.async_set_water_speed(params)
         except Exception:
             _LOGGER.error("Unsupported raw command")
